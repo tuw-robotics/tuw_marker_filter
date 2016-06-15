9c2741ce7a24f35f51af1d577ff39c3d8ed127b6
mmacsek <markus.macsek@student.tuwien.ac.at>
2016-06-15 11:26:06 +0200

    Added missing measurement marker files

---
+++ tuw_slam/include/tuw_slam/measurement_marker.h
@@ -0,0 +1,125 @@
+#ifndef MEASUREMENT_MARKER_H
+#define MEASUREMENT_MARKER_H
+
+#include <vector>
+#include <tuw_geometry/pose2d.h>
+#include <tuw_geometry/measurement.h>
+
+namespace tuw {
+
+class MeasurementMarker;  /// Prototype
+using MeasurementMarkerPtr =  std::shared_ptr< MeasurementMarker >;
+using MeasurementMarkerConstPtr = std::shared_ptr< MeasurementMarker const >; 
+
+/**
+ * class to represent marker measurements
+ **/
+class MeasurementMarker : public Measurement{
+public:
+    /**
+    * class to represent a single marker with redundant information
+    **/
+    struct Marker {
+        int id;
+
+        double length;
+        double angle;
+        double orientation;
+        Pose2D pose;
+    };
+    /**
+     * constructor
+     * @param distinguish extended classes in base versions
+     **/
+    MeasurementMarker();
+    /**
+     * copy constructor
+     * @param o source
+     **/
+    MeasurementMarker(const MeasurementMarker &o);
+    /**
+     * resizes the vector holding the marker measurements
+     * @param size
+     **/
+    void resize ( size_t n );
+    /**
+     * returns the min angle measurement
+     * @return possible min measurement
+     **/
+    double &angle_min();
+    /**
+     * returns the min angle measurement
+     * @return possible min measurement
+     **/
+    const double &angle_min() const;
+    /**
+     * returns the max angle measurement
+     * @return possible max measurement
+     **/
+    double &angle_max();
+    /**
+     * returns the max angle measurement
+     * @return possible max measurement
+     **/
+    const double &angle_max() const;
+    /**
+     * returns the min range measurement
+     * @return possible min measurement
+     **/
+    double &range_min();
+    /**
+     * returns the min range measurement
+     * @return possible min measurement
+     **/
+    const double &range_min() const;
+    /**
+     * returns the max range measurement
+     * @return possible max measurement
+     **/
+    double &range_max();
+    /**
+     * returns the max range measurement
+     * @return possible max measurement
+     **/
+    const double &range_max() const;
+    /**
+     * returns the max id range measurement
+     * @return possible max id measurement
+     **/
+    double &range_max_id();
+    /**
+     * returns the max id range measurement
+     * @return possible max id measurement
+     **/
+    const double &range_max_id() const;
+    /**
+     * @return true on empty
+     **/
+    bool empty() const;
+    /**
+     * @return number of markers
+     **/
+    size_t size() const;
+    /**
+     * @param i idx
+     * @return a single marker
+     **/
+    Marker& operator[] ( int i );
+    /**
+     * @param i idx
+     * @return a single marker
+     **/
+    const Marker& operator[] ( int i ) const;
+private:
+    double angle_min_;
+    double angle_max_;
+
+    double range_max_;
+    double range_min_;
+    double range_max_id_;
+
+    std::vector<Marker> markers_;
+};
+};
+
+#endif 
---
+++ tuw_slam/src/tuw_slam/measurement_marker.cpp
@@ -0,0 +1,57 @@
+#include <tuw_slam/measurement_marker.h>
+using namespace tuw;
+
+MeasurementMarker::MeasurementMarker()
+    :Measurement ( Type::MARKER ) {
+}
+MeasurementMarker::MeasurementMarker ( const MeasurementMarker& o )
+    :Measurement ( o ), range_max_id_ ( o.range_max_id_ ), range_max_ ( o.range_max_ ), range_min_ ( o.range_min_ ), markers_ ( o.markers_ ) {
+}
+void MeasurementMarker::resize ( size_t n ) {
+    markers_.resize ( n );
+}
+double &MeasurementMarker::angle_min() {
+    return angle_min_;
+}
+double &MeasurementMarker::angle_max() {
+    return angle_max_;
+}
+double &MeasurementMarker::range_min() {
+    return range_min_;
+}
+const double &MeasurementMarker::range_min() const {
+    return range_min_;
+}
+double &MeasurementMarker::range_max() {
+    return range_max_;
+}
+const double &MeasurementMarker::range_max() const {
+    return range_max_;
+}
+double &MeasurementMarker::range_max_id() {
+    return range_max_id_;
+}
+const double &MeasurementMarker::range_max_id() const {
+    return range_max_id_;
+}
+bool MeasurementMarker::empty() const {
+    return markers_.empty();
+}
+size_t MeasurementMarker::size() const {
+    return markers_.size();
+}
+/** Array operation
+ * @param i entry to return
+ * @return reference to the element
+ **/
+MeasurementMarker::Marker& MeasurementMarker::operator[] ( int i ) {
+    return markers_[i];
+}
+/** Array operation const version
+ * @param i entry to return
+ * @return reference to the element
+ **/
+const MeasurementMarker::Marker& MeasurementMarker::operator[] ( int i ) const {
+    return markers_[i];
+}
+
