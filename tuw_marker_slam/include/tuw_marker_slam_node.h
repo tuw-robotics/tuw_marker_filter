b2950953c324d3124831aa87f40e3df64e9d823b
mmacsek <markus.macsek@student.tuwien.ac.at>
2016-06-17 18:06:34 +0200

    Changed message type of estimated pose to geometry_msgs::PoseWithCovarianceStamped.msg and changed published transformations in accordance to amcl transformation publishing

--- tuw_marker_slam/include/tuw_marker_slam_node.h
+++ tuw_marker_slam/include/tuw_marker_slam_node.h
@@ -4,16 +4,10 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
-#include <geometry_msgs/Pose2D.h>
-#include <geometry_msgs/PoseArray.h>
-#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
 #include <marker_msgs/MarkerDetection.h>
-#include <marker_msgs/MarkerWithCovarianceStamped.h>
 #include <marker_msgs/MarkerWithCovarianceArray.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <dynamic_reconfigure/server.h>
 
 #include "tuw_marker_slam/tuw_marker_slam.h"
@@ -31,22 +25,23 @@ public:
 private:
     ros::NodeHandle n_;                                                                                 /// node handler to the root node
     ros::NodeHandle n_param_;                                                                           /// node handler to the current node
+
     ros::Subscriber sub_cmd_;                                                                           /// Subscriber to the command measurements
     ros::Subscriber sub_marker_;                                                                        /// Subscriber to the marker detector
-    ros::Subscriber sub_ground_truth_;                                                                  /// Subscriber to the ground truth pose (simulation only)
+
     ros::Publisher pub_xt_;                                                                             /// publisher for the estimated robot pose and its variance
     ros::Publisher pub_mt_;                                                                             /// publisher for the estimated landmark poses and their variances
-    tf::TransformBroadcaster tf_broadcaster_;                                                           /// broadcasts transformation messages
-    std::shared_ptr<tf::TransformListener> tf_listener_;                                                /// listener to receive transformation messages -> to get the marker detector pose
-    marker_msgs::MarkerWithCovarianceStamped xt_;                                                       /// estimated robot pose and its variance to publish
+    geometry_msgs::PoseWithCovarianceStamped xt_;                                                       /// estimated robot pose and its variance to publish
     marker_msgs::MarkerWithCovarianceArray mt_;                                                         /// estimated landmark poses and their variances to publish
 
-    tuw::Pose2D pose_ground_truth_;                                                                     /// ground truth state x, y, alpha (simulation only)
-    tuw::Pose2D origin_;                                                                                /// origin x, y, alpha of the slam created map in the world (simulation only)
+    tf::TransformBroadcaster tf_broadcaster_;                                                           /// broadcasts transformation messages
+    std::shared_ptr<tf::TransformListener> tf_listener_;                                                /// listener to receive transformation messages -> to get the marker detector pose
+    std::string frame_id_map_;                                                                          /// frame id of map (for transformations)
+    std::string frame_id_odom_;                                                                         /// frame id of odom (for transformations)
+    std::string frame_id_base_;                                                                         /// frame id of base (for transformations)
 
     void callbackCmd ( const geometry_msgs::Twist& );                                                   /// callback function to catch motion commands
     void callbackMarker ( const marker_msgs::MarkerDetection& );                                        /// callback function to catch incoming sensor data
-    void callbackGroundTruth ( const nav_msgs::Odometry& );                                             /// callback function to catch ground truth pose messages
 
     dynamic_reconfigure::Server<tuw_marker_slam::SLAMConfig> reconfigureServerSLAM_;                           /// parameter server stuff general use
     dynamic_reconfigure::Server<tuw_marker_slam::SLAMConfig>::CallbackType reconfigureFncSLAM_;                /// parameter server stuff general use
--- tuw_marker_slam/src/tuw_marker_slam_node.cpp
+++ tuw_marker_slam/src/tuw_marker_slam_node.cpp
@@ -34,9 +34,15 @@ SLAMNode::SLAMNode ( ros::NodeHandle & n )
     : SLAM (),
       n_ ( n ),
       n_param_ ( "~" ) {
-    // read in parameters
+    // read in common parameters
     int mode;
     n_param_.getParam ( "mode", mode );
+    n_param_.param<std::string> ( "frame_id_map", frame_id_map_, "map" );
+    n_param_.param<std::string> ( "frame_id_odom", frame_id_odom_, "odom" );
+    n_param_.param<std::string> ( "frame_id_base", frame_id_base_, "base_link" );
+    tf::resolve ( n_.getNamespace(), frame_id_map_ );
+    tf::resolve ( n_.getNamespace(), frame_id_odom_ );
+    tf::resolve ( n_.getNamespace(), frame_id_base_ );
 
     switch ( mode ) {
     case SLAMTechnique::EKF:
@@ -50,7 +56,7 @@ SLAMNode::SLAMNode ( ros::NodeHandle & n )
         n_param_.getParam ( "beta_6", beta_6 );
 
         zt_ = std::make_shared<tuw::MeasurementMarker>();
-        slam_technique_ = std::make_shared<tuw::EKFSLAM>( beta_1, beta_2, beta_3, beta_4, beta_5, beta_6 );
+        slam_technique_ = std::make_shared<tuw::EKFSLAM> ( beta_1, beta_2, beta_3, beta_4, beta_5, beta_6 );
         break;
     default:
         ROS_ERROR ( "[%s] mode %i is not supported", ros::this_node::getName().c_str(), mode );
@@ -58,31 +64,21 @@ SLAMNode::SLAMNode ( ros::NodeHandle & n )
     }
     ROS_INFO ( "[%s] mode: %s (%i)", ros::this_node::getName().c_str(), slam_technique_->getTypeName().c_str(), ( int ) slam_technique_->getType() );
 
-    // initialize global variables
-    //pose_ground_truth_ = Pose2D ( -3.0, -3.0, M_PI/4 );
-    //pose_ground_truth_ = Pose2D ( 0, -2, 0 );
-    pose_ground_truth_ = Pose2D ( 0, -5, 0 );
-    //pose_ground_truth_ = Pose2D ( 0, -8, 0 );
-    origin_ = pose_ground_truth_;
-
-    /// subscribes to transforamtions
-    tf_listener_ = std::make_shared<tf::TransformListener>();
-
     /// subscribes to command values
     sub_cmd_ = n.subscribe ( "cmd", 1, &SLAMNode::callbackCmd, this );
 
-    /// subscribes to ground truth data
-    sub_ground_truth_ = n.subscribe ( "base_pose_ground_truth", 1, &SLAMNode::callbackGroundTruth, this );
-
     /// defines publishers for the resulting robot pose
-    pub_xt_ = n_param_.advertise<marker_msgs::MarkerWithCovarianceStamped> ( "xt", 1 );
-    n_param_.param<std::string> ( "frame_id_map", xt_.header.frame_id, "map" );
+    pub_xt_ = n_param_.advertise<geometry_msgs::PoseWithCovarianceStamped> ( "xt", 1 );
+    xt_.header.frame_id = frame_id_map_;
     xt_.header.seq = 0;
 
     /// defines publishers for the resulting landmark poses
     pub_mt_ = n_param_.advertise<marker_msgs::MarkerWithCovarianceArray> ( "mt", 1 );
-    n_param_.param<std::string> ( "frame_id_map", mt_.header.frame_id, "map" );
+    mt_.header.frame_id = frame_id_map_;
     mt_.header.seq = 0;
+
+    /// subscribes to transforamtions
+    tf_listener_ = std::make_shared<tf::TransformListener>();
 
     /// start parameter server
     reconfigureFncSLAM_ = boost::bind ( &SLAMNode::callbackConfigSLAM, this,  _1, _2 );
@@ -105,7 +101,6 @@ SLAMNode::SLAMNode ( ros::NodeHandle & n )
 
 void SLAMNode::cycle() {
     if ( config_.reset ) {
-        origin_ = pose_ground_truth_;
         slam_technique_->reset();
     }
 
@@ -118,23 +113,19 @@ void SLAMNode::cycle() {
 void SLAMNode::publish () {
     if ( slam_technique_->time_last_update().is_not_a_date_time() ) return;
 
-    // transformations
-    tf::Quaternion q;
-    tf::Transform trans;
+    // Broadast transformation map to odom
+    tf::Transform base_to_map;
+    tf::Stamped<tf::Pose> map_to_base;
+    tf::Stamped<tf::Pose> odom_to_map;
+    tf::StampedTransform map_to_odom;
 
-    // publish world -> map transformation
-    std::string map;
-    n_param_.param<std::string> ( "frame_id_map", map, "map" );
-    q.setRPY ( 0.0, 0.0, origin_.theta() );
-    trans =  tf::Transform ( q, tf::Point ( origin_.x(), origin_.y(), 0 ) );
-    tf_broadcaster_.sendTransform ( tf::StampedTransform ( trans, ros::Time::fromBoost ( slam_technique_->time_last_update() ), "odom", map ) );
+    // subtracting base to odom from map to base (cp. http://wiki.ros.org/amcl)
+    base_to_map = tf::Transform ( tf::createQuaternionFromYaw ( yt_[0].theta() ), tf::Point ( yt_[0].x(), yt_[0].y(), 0 ) );
+    map_to_base = tf::Stamped<tf::Pose> ( base_to_map.inverse(), ros::Time::fromBoost ( slam_technique_->time_last_update() ), frame_id_base_ );
+    tf_listener_->transformPose ( frame_id_odom_, map_to_base, odom_to_map );
+    map_to_odom = tf::StampedTransform ( odom_to_map.inverse(), odom_to_map.stamp_, frame_id_map_, frame_id_odom_ );
 
-    // publish map -> estimated pose transforamtion
-    std::string slam_pose;
-    n_param_.param<std::string> ( "frame_id_slam_pose", slam_pose, "slam_pose" );
-    q.setRPY ( 0.0, 0.0, yt_[0].theta() );
-    trans =  tf::Transform ( q, tf::Point (yt_[0].x(), yt_[0].y(), 0 ) );
-    tf_broadcaster_.sendTransform ( tf::StampedTransform ( trans, ros::Time::fromBoost ( slam_technique_->time_last_update() ), map, slam_pose ) );
+    tf_broadcaster_.sendTransform ( map_to_odom );
 
     assert ( yt_.size() > 0 && C_Yt_.rows == 3*yt_.size() && C_Yt_.cols == 3*yt_.size() );
 
@@ -142,21 +133,21 @@ void SLAMNode::publish () {
     xt_.header.stamp = ros::Time::fromBoost ( slam_technique_->time_last_update() );
     xt_.header.seq++;
 
-    xt_.marker.marker.pose.position.x = yt_[0].x();
-    xt_.marker.marker.pose.position.y = yt_[0].y();
-    xt_.marker.marker.pose.position.z = 0;
-    xt_.marker.marker.pose.orientation = tf::createQuaternionMsgFromYaw ( yt_[0].theta() );
+    xt_.pose.pose.position.x = yt_[0].x();
+    xt_.pose.pose.position.y = yt_[0].y();
+    xt_.pose.pose.position.z = 0;
+    xt_.pose.pose.orientation = tf::createQuaternionMsgFromYaw ( yt_[0].theta() );
 
     cv::Matx<double, 3, 3> C_X2 = cv::Mat_<double> ( C_Yt_, cv::Range ( 0, 3 ), cv::Range ( 0, 3 ) );
-    xt_.marker.covariance[6*0 + 0] = C_X2(0, 0);
-    xt_.marker.covariance[6*0 + 1] = C_X2(0, 1);
-    xt_.marker.covariance[6*0 + 5] = C_X2(0, 2);
-    xt_.marker.covariance[6*1 + 0] = C_X2(1, 0);
-    xt_.marker.covariance[6*1 + 1] = C_X2(1, 1);
-    xt_.marker.covariance[6*1 + 5] = C_X2(1, 2);
-    xt_.marker.covariance[6*5 + 0] = C_X2(2, 0);
-    xt_.marker.covariance[6*5 + 1] = C_X2(2, 1);
-    xt_.marker.covariance[6*5 + 5] = C_X2(2, 2);
+    xt_.pose.covariance[6*0 + 0] = C_X2 ( 0, 0 );
+    xt_.pose.covariance[6*0 + 1] = C_X2 ( 0, 1 );
+    xt_.pose.covariance[6*0 + 5] = C_X2 ( 0, 2 );
+    xt_.pose.covariance[6*1 + 0] = C_X2 ( 1, 0 );
+    xt_.pose.covariance[6*1 + 1] = C_X2 ( 1, 1 );
+    xt_.pose.covariance[6*1 + 5] = C_X2 ( 1, 2 );
+    xt_.pose.covariance[6*5 + 0] = C_X2 ( 2, 0 );
+    xt_.pose.covariance[6*5 + 1] = C_X2 ( 2, 1 );
+    xt_.pose.covariance[6*5 + 5] = C_X2 ( 2, 2 );
 
     pub_xt_.publish ( xt_ );
 
@@ -172,15 +163,15 @@ void SLAMNode::publish () {
         mt_.markers[i].marker.pose.orientation = tf::createQuaternionMsgFromYaw ( yt_[i+1].theta() );
 
         cv::Matx<double, 3, 3> C_Mi2 = cv::Mat_<double> ( C_Yt_, cv::Range ( 3* ( i+1 ), 3* ( i+1 ) + 3 ), cv::Range ( 3* ( i+1 ), 3* ( i+1 ) + 3 ) );
-        mt_.markers[i].covariance[6*0 + 0] = C_Mi2(0, 0);
-        mt_.markers[i].covariance[6*0 + 1] = C_Mi2(0, 1);
-        mt_.markers[i].covariance[6*0 + 5] = C_Mi2(0, 2);
-        mt_.markers[i].covariance[6*1 + 0] = C_Mi2(1, 0);
-        mt_.markers[i].covariance[6*1 + 1] = C_Mi2(1, 1);
-        mt_.markers[i].covariance[6*1 + 5] = C_Mi2(1, 2);
-        mt_.markers[i].covariance[6*5 + 0] = C_Mi2(2, 0);
-        mt_.markers[i].covariance[6*5 + 1] = C_Mi2(2, 1);
-        mt_.markers[i].covariance[6*5 + 5] = C_Mi2(2, 2);
+        mt_.markers[i].covariance[6*0 + 0] = C_Mi2 ( 0, 0 );
+        mt_.markers[i].covariance[6*0 + 1] = C_Mi2 ( 0, 1 );
+        mt_.markers[i].covariance[6*0 + 5] = C_Mi2 ( 0, 2 );
+        mt_.markers[i].covariance[6*1 + 0] = C_Mi2 ( 1, 0 );
+        mt_.markers[i].covariance[6*1 + 1] = C_Mi2 ( 1, 1 );
+        mt_.markers[i].covariance[6*1 + 5] = C_Mi2 ( 1, 2 );
+        mt_.markers[i].covariance[6*5 + 0] = C_Mi2 ( 2, 0 );
+        mt_.markers[i].covariance[6*5 + 1] = C_Mi2 ( 2, 1 );
+        mt_.markers[i].covariance[6*5 + 5] = C_Mi2 ( 2, 2 );
     }
 
     pub_mt_.publish ( mt_ );
@@ -205,10 +196,10 @@ void SLAMNode::callbackMarker ( const marker_msgs::MarkerDetection &_marker ) {
 
     try {
         tf::StampedTransform transform;
-        tf_listener_->lookupTransform ( tf::resolve ( n_.getNamespace(),"base_link" ), _marker.header.frame_id, ros::Time ( 0 ), transform );
+        tf_listener_->lookupTransform ( frame_id_base_, _marker.header.frame_id, ros::Time ( 0 ), transform );
         zt->pose2d() = Pose2D ( transform.getOrigin().getX(),
-                                       transform.getOrigin().getY(),
-                                       tf::getYaw ( transform.getRotation() ) );
+                                transform.getOrigin().getY(),
+                                tf::getYaw ( transform.getRotation() ) );
     } catch ( tf::TransformException &ex ) {
         ROS_ERROR ( "[%s callbackMarker] %s", ros::this_node::getName().c_str(), ex.what() );
         zt->pose2d() = Pose2D ( 0.225,  0, 0 );
@@ -220,6 +211,7 @@ void SLAMNode::callbackMarker ( const marker_msgs::MarkerDetection &_marker ) {
     } else {
         ROS_ERROR ( "[%s callbackMarker] %s", ros::this_node::getName().c_str(), "This node only deals with straight forward looking view directions" );
     }
+
     zt->range_min() = _marker.distance_min;
     zt->range_max() = _marker.distance_max;
     zt->range_max_id() = _marker.distance_max_id;
@@ -238,20 +230,6 @@ void SLAMNode::callbackMarker ( const marker_msgs::MarkerDetection &_marker ) {
         zt->operator[] ( i ).orientation = orientation;
         zt->operator[] ( i ).pose = Pose2D ( v.getX(), v.getY(), orientation );
     }
-}
-
-/**
- * copies incoming odemetry messages to the base class
- * @param odom
- **/
-void SLAMNode::callbackGroundTruth ( const nav_msgs::Odometry& ground_truth ) {
-    tf::Quaternion q;
-    double roll = 0, pitch = 0, yaw = 0;
-
-    tf::quaternionMsgToTF ( ground_truth.pose.pose.orientation, q );
-    tf::Matrix3x3 ( q ).getRPY ( roll, pitch, yaw );
-
-    pose_ground_truth_.set ( ground_truth.pose.pose.position.x, ground_truth.pose.pose.position.y, yaw );
 }
 
 void SLAMNode::callbackConfigSLAM ( tuw_marker_slam::SLAMConfig &config, uint32_t level ) {
