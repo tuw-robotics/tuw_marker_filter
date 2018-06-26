Particle filter weight calculation correct. Resampling also.

tuw_feature_slam/modules/tuw/particle_filter_localization.py
@@ -23,9 +23,48 @@ def angle_difference(alpha0, angle1):
 #         self.angle = 0.0
 #         self.end_point = np.array([0, 0], np.double)
 
+class Match:
+
+    def __init__(self,obs_id, map_id, distance):
+        self.observation_id = obs_id
+        self.map_id = map_id
+        self.distance = distance
+
+    def __str__(self):
+        return "observation {} -> {} marker, dist: {}".format(self.observation_id, self.map_id, self.distance)
+
+class MatchInfo:
+
+    def __init__(self):
+        self.sample_to_match = {}
+
+    def add_match(self, s, obs_id, map_id, distance):
+        if s.get_idx() in self.sample_to_match:
+            self.sample_to_match[s.get_idx()].append(Match(obs_id, map_id, distance))
+        else:
+            self.sample_to_match[s.get_idx()] = []
+            self.sample_to_match[s.get_idx()].append(Match(obs_id, map_id, distance))
+
+    def print_best_sample(self):
+        best_dist = np.finfo(np.double).max
+        best_idx = 0
+        for s_idx, match_obj_list in self.sample_to_match.iteritems():
+            dist_sum = 0.0
+            for match_obj in match_obj_list:
+                dist_sum += match_obj.distance
+            dist_sum = dist_sum / len(match_obj_list)
+            if dist_sum < best_dist:
+                best_dist = dist_sum
+                best_idx = s_idx
+        print("best particle id: {}".format(best_idx))
+        print("Match info:")
+        for match_obj in self.sample_to_match[best_idx]:
+            print("{}".format(match_obj.__str__()))
+
 class Sample:
 
-    def __init__(self, pose=None, orientation=None, weight=None, x=None, y=None):
+    def __init__(self, idx, pose=None, orientation=None, weight=None, x=None, y=None):
+        self.idx = idx
         if pose is None:
             self.pose = np.array([[0], [0]], np.double)
         else:
@@ -41,9 +80,22 @@ class Sample:
         else:
             self.weight = weight
         self.hit = False
+        self.obs_id_to_marker_id = {}
+
+    def get_idx(self):
+        return self.idx
+
+    def set_idx(self, idx):
+        self.idx = idx
 
     def clone(self):
-        return Sample(self.pose.copy(), self.orientation, self.weight)
+        return Sample(self.idx, self.pose.copy(), self.orientation, self.weight)
+
+    def set_observation_marker_link(self, o_id, m_id):
+        self.obs_id_to_marker_id[o_id] = m_id
+
+    def get_observation_marker_links(self):
+        return self.obs_id_to_marker_id
 
     def set_hit(self, val):
         self.hit = val
@@ -103,13 +155,13 @@ class Vehicle(object):
         self.alpha3 = 0.1
         self.alpha4 = 0.1
         self.alpha5 = 0.1
-        self.nr_of_samples = 512
+        self.nr_of_samples = 128
         self.resample_rate = 0.25
         self.sample_mode = 2  # 1,2
         self.sigma_static_position = 0.05
         self.sigma_static_orientation = 0.05
         self.enable_weighting = True
-        self.enable_resample = False
+        self.enable_resample = True
         self.enable_update = True
         self.sigma_init_position = 0.10
         self.sigma_init_orientation = 0.10
@@ -120,20 +172,24 @@ class Vehicle(object):
         if self.is_initialized:
             return
         angle_division = 4
-        i = 0
-        samples_per_angle = self.nr_of_samples/angle_division
+        samples_per_angle = self.nr_of_samples / angle_division
         A = (xrange[1] - xrange[0]) * (yrange[1] - yrange[0])
         samples_per_m2 = np.double(samples_per_angle) / np.double(A)
         d = 1.0 / np.sqrt(samples_per_m2)
-        #d = 0.65
         del self.samples[:]
-        xxrange = np.arange(xrange[0] + d/2.0, xrange[1],d)
-        yyrange = np.arange(yrange[0] + d/2.0, yrange[1],d)
+        xxrange = np.arange(xrange[0] + d / 2.0, xrange[1], d)
+        yyrange = np.arange(yrange[0] + d / 2.0, yrange[1], d)
         angle_range = np.arange(-np.pi, np.pi, (2.0 * np.pi) / angle_division)
+        s_id = 0
         for x_in in xxrange:
             for y_in in yyrange:
                 for theta_in in angle_range:
-                    self.samples.append(Sample(x=x_in,y=y_in,orientation=theta_in,weight=1.0))
+                    self.samples.append(Sample(idx=s_id,
+                                               x=x_in,
+                                               y=y_in,
+                                               orientation=theta_in,
+                                               weight=1.0))
+                    s_id += 1
         self.nr_of_samples = len(self.samples)
         print("{} samples created.".format(len(self.samples)))
         self.is_initialized = True
@@ -147,12 +203,13 @@ class Vehicle(object):
         for i in range(self.nr_of_samples):
             init_pose = self.x[:2, 0] + np.random.normal(0.0, self.sigma_init_position, (2, 1))
             init_orientation = self.x[2, 0] + np.random.normal(0.0, self.sigma_init_orientation)
-            self.samples.append(Sample(pose=init_pose,
+            self.samples.append(Sample(idx=i,
+                                       pose=init_pose,
                                        orientation=init_orientation,
                                        weight=1.0))
         self.is_initialized = True
 
-    def nearest_marker(self, end_point, tolerance=0.8):
+    def nearest_marker(self, end_point, tolerance=5000):
         best_marker_dist = np.finfo(np.double).max
         best_marker_id = 0
         for i in range(self.m.shape[0]):
@@ -162,21 +219,33 @@ class Vehicle(object):
             x = end_point[0, 0]
             y = end_point[1, 0]
             theta = end_point[2, 0]
-            distance = np.sqrt((x_m - x) * (x_m - x) + (y_m - y) * (y_m - y) + (theta_m - theta) * (theta_m - theta))
+            distance = np.sqrt((x_m - x) * (x_m - x) + (y_m - y) * (y_m - y))
             if distance < best_marker_dist:
                 best_marker_dist = distance
-                best_marker_id = i
+                best_marker_idx = i
+                best_marker_id = self.m[i,0]
         if best_marker_dist < tolerance:
-            return True, best_marker_id, [self.m[best_marker_id, 1], self.m[best_marker_id, 2]], best_marker_dist
+            return True, best_marker_id, [self.m[best_marker_idx, 1], self.m[best_marker_idx, 2]], best_marker_dist
         else:
-            return False, best_marker_id, [self.m[best_marker_id, 1], self.m[best_marker_id, 2]], best_marker_dist
+            return False, best_marker_id, [self.m[best_marker_idx, 1], self.m[best_marker_idx, 2]], best_marker_dist
 
     def set_odom(self, pose):
         if hasattr(self, 'x') == False:
             self.x = pose
             self.init_samples()
         self.odom = pose
-        #self.x = pose
+        # self.x = pose
+
+    def normal(self, s, dt):
+        s_position = s.get_position()
+
+        # normalization
+        s_position[0, 0] = s_position[0, 0] + np.random.normal(0.0, self.sigma_static_position * dt)
+        s_position[1, 0] = s_position[1, 0] + np.random.normal(0.0, self.sigma_static_position * dt)
+        s_orientation = s.get_orientation() + np.random.normal(0.0, self.sigma_static_orientation * dt)
+
+        s.set_position(s_position)
+        s.set_orientation(s_orientation)
 
     def define_map(self, m):
         self.m = m
@@ -241,27 +310,33 @@ class Vehicle(object):
         return True
 
     def weighting(self, z):
+        match_info = MatchInfo()
+        s_idx = 0
         for s in self.samples:
-            s.set_hit(True)
+            s.set_hit(False)
             q = 1.0
             qr = self.laser_z_rand / self.laser_z_max
-            for observation in z:
-                end_point_ws = observation[0,1:4].reshape(3,1)  # TODO: check if transform is correct and also in which coordinate system the beams endpoint is given
-                end_point_ws = transform_pose(s.get_pose().reshape(3, 1), end_point_ws)
-                has_marker, id, marker_position, dist = self.nearest_marker(end_point_ws)
+            for o_id, observation in enumerate(z):
+                end_point_ws = observation[0, 1:4].reshape(3,1).copy()  # TODO: check if transform is correct and also in which coordinate system the beams endpoint is given
+                end_point_ws = transform_pose(end_point_ws.reshape(3,1), s.get_pose().reshape(3, 1))
+                has_marker, map_id, marker_position, dist = self.nearest_marker(end_point_ws)
                 qh = 0.0
                 if has_marker:
-                    gauss_sample = scipy.stats.norm.pdf(dist, self.laser_z_hit)
+                    s.set_hit(True)
+                    gauss_sample = scipy.stats.norm.pdf(dist, 0.0, self.laser_z_hit)
                     if gauss_sample < 0.0:
                         raise ValueError("gauss sample < 0.0 dist: {}, sample: {}".format(dist, gauss_sample))
                     qh = self.laser_z_hit * gauss_sample
+                    s.set_observation_marker_link(o_id, map_id)
+                    match_info.add_match(s,o_id,map_id,dist)
                 if (qr + qh) < 0.0:
-                    raise ValueError("{} + {} < 0.0".format(qr,qh))
+                    raise ValueError("qr + qh = {} + {} < 0.0".format(qr, qh))
                 q *= (qr + qh)
-                #if not has_marker:
+                # if not has_marker:
                 #    s.set_weight(s.get_weight() * (self.laser_z_rand / self.laser_z_max))
                 #    continue
             s.set_weight(q)
+            s_idx += 1
         self.samples.sort(key=lambda x: x.get_weight(), reverse=True)
         weight_sum = 0.0
         for s in self.samples:
@@ -272,31 +347,30 @@ class Vehicle(object):
             s.set_weight(s.get_weight() / weight_sum)
             self.weight_max = np.max([s.get_weight(), self.weight_max])
             self.weight_min = np.min([s.get_weight(), self.weight_min])
-        self.x = self.samples[0].get_pose().reshape(3,1)
+        self.x = self.samples[0].get_pose().reshape(3, 1)
         print("max weight: {}".format(self.weight_max))
         print("min weight: {}".format(self.weight_min))
-        #print("odom pose: {}".format(self.odom))
-        #print("estimated pose: {}".format(self.x))
-        #print("best particle pose: {}".format(self.samples[0].get_pose()))
+        print("self.particles[0] id: {}".format(self.samples[0].get_idx()))
+        #just for visualization purpose
+        for k,v in self.samples[0].get_observation_marker_links().iteritems():
+            z[k, 0] = v
+        # print("odom pose: {}".format(self.odom))
+        # print("estimated pose: {}".format(self.x))
+        # print("best particle pose: {}".format(self.samples[0].get_pose()))
 
     def resample(self):
         dt = self.dt
         M = np.floor(self.nr_of_samples * self.resample_rate)
-
+        #assumption: self.samples are sorted based on weight in decreasing order
         if self.sample_mode == 1:
             idx = 0
             while idx < M:
                 s = self.samples[idx]
-                self.samples[len(self.samples) - idx - 1] = s
-                s_position = s.get_position()
+                s_cloned = s.clone()
+                self.samples[len(self.samples) - idx - 1] = s_cloned
+                s_cloned.set_idx(len(self.samples) - idx - 1)
+                self.normal(s_cloned, dt)
 
-                # normalization
-                s_position[0, 0] = s_position[0, 0] + np.random.normal(0.0, self.sigma_static_position * dt)
-                s_position[1, 0] = s_position[1, 0] + np.random.normal(0.0, self.sigma_static_position * dt)
-                s.set_position(s_position)
-
-                s_orientation = s.get_orientation() + np.random.normal(0.0, self.sigma_static_orientation * dt)
-                s.set_orientation(s_orientation)
                 idx += 1
         if self.sample_mode == 2:
             if self.samples[0].get_weight() <= 0.00001:
@@ -312,21 +386,24 @@ class Vehicle(object):
                 while u > c and i < M:
                     c = c + self.samples[i].get_weight()
                     i = i + 1
-
                 # normalization
                 s = self.samples[i]
                 s_new = s.clone()
-                s_position = s.get_position()
+                s_new.set_idx(len(samples_new))
 
-                s_position[0, 0] = s_position[0, 0] + np.random.normal(0.0, 1.0) * self.sigma_static_position * dt
-                s_position[1, 0] = s_position[1, 0] + np.random.normal(0.0, 1.0) * self.sigma_static_position * dt
-                s_new.set_position(s_position)
+                self.normal(s_new, dt)
 
-                s_orientation = s.get_orientation() + np.random.normal(0.0, 1.0) * self.sigma_static_orientation * dt
-                s_new.set_orientation(s_orientation)
-
-                samples_new.append(s)
+                samples_new.append(s_new)
             self.samples = samples_new
+
+            if self.nr_of_samples < len(self.samples):
+                del self.samples[(self.nr_of_samples-1):]
+            while self.nr_of_samples > len(self.samples):
+                parent = self.samples[np.random.randint(0,len(self.samples))]
+                child = parent.clone()
+                self.normal(child, dt)
+                child.set_idx(len(self.samples))
+                self.samples.append(child)
             print("new particle size: {}".format(len(self.samples)))
         else:
             pass

tuw_feature_slam/modules/tuw/plot.py
@@ -127,12 +127,12 @@ class Landmark(Polygon):
 
 class ParticleCircle(Circle):
 
-    def __init__(self, pose, theta, color, **kwargs):
-        Circle.__init__(self, np.array([0, 0]), 0.2, **kwargs)
+    def __init__(self, pose, theta, color, transparency, **kwargs):
+        Circle.__init__(self, np.array([pose.item(0), pose.item(1)]),
+                        0.2, **kwargs)
         self.set_edgecolor(color)
         self.set_facecolor('none')
-        #self.set_alpha(transparency)
-        self.center = np.array([pose.item(0), pose.item(1)])
+        self.set_alpha(transparency)
         self.angle = theta
 
     def reset(self, pose, weight):

tuw_feature_slam/modules/tuw/vehicle_plot.py
@@ -31,7 +31,7 @@ class VehiclePlot:
         ax.add_artist(self.PoseArrowOdom)
         self.PoseArrowTruePose = PoseArrow(0.4, 'k', 0.4)
         ax.add_artist(self.PoseArrowTruePose)
-        self.PoseArrowRobot = PoseArrow(0.4, 'r', 0.4)
+        self.PoseArrowRobot = PoseArrow(0.4, 'm', 0.4)
         self.CovEllipseRobot = CovEllipse('r', 0.4)
         ax.add_artist(self.PoseArrowRobot)
         ax.add_artist(self.CovEllipseRobot)
@@ -55,12 +55,14 @@ class VehiclePlot:
         for s in self.vehicle.samples:
             s_p = s.get_pose()
             transparency = 0.2 + 0.8 * (s.get_weight() * scale)
-            arrow = PoseArrow(0.5, np.array([s.get_weight() * scale, 1.0 - (s.get_weight() * scale), 0.0]),
-                              transparency)
-            arrow.set_pose(s_p)
-            arrow.set_zorder(n_samples_to_process)
+            #arrow = PoseArrow(0.5, np.array([s.get_weight() * scale, 1.0 - (s.get_weight() * scale), 0.0]),
+            #                 transparency)
+            artist = ParticleCircle(s_p,s_p.item(2),np.array([s.get_weight() * scale,
+                                                              1.0 - (s.get_weight() * scale),
+                                                              0.0]),transparency)
+            artist.set_zorder(n_samples_to_process)
             n_samples_to_process = n_samples_to_process - 1
-            self.sample_artists.append(arrow)
+            self.sample_artists.append(artist)
             self.ax.add_artist(self.sample_artists[-1])
 
     def set_odom(self, pose):

tuw_feature_slam/skripts/vehicle_demo_particle_filter.py
@@ -26,7 +26,7 @@ ax.set_ylim([-12, 12])
 plt.ylabel('y')
 plt.show()
 
-VISUALIZATION_MODE = True
+VISUALIZATION_MODE = False
 
 def loop(vehicle, filename):
     skip = 10
@@ -57,7 +57,6 @@ def loop(vehicle, filename):
                         z[i, 0] = z[i, 0] - 1
                 vehicle.measurments(z)
                 fig.canvas.draw()
-                time.sleep(5)
                 # print (line)
             if ('map' == header):
                 t = np.matrix(map(int, elements[1].split(",")))